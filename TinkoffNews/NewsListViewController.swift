601c357ab9c1dd112445642f64ee3938f61e480f
Nervanin <Volua53@yandex.ru>
2019-05-15 21:30:42 +0300

    init commit

---
+++ TinkoffNews/NewsListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TinkoffNews
+//
+//  Created by Konstantin on 15/05/2019.
+//  Copyright © 2019 Konstantin Meleshko. All rights reserved.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        
+    }
+
+
+}
+
---
+++ TinkoffNews/TableViewDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewDelegate.swift
+//  TinkoffNews
+//
+//  Created by Konstantin on 15/05/2019.
+//  Copyright © 2019 Konstantin Meleshko. All rights reserved.
+//
+
+import Foundation
